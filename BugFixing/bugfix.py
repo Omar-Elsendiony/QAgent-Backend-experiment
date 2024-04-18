c117ed9a7734c849f9fd34005e0eef5abd60396e
karimyasser107 <karimyasser34@gmail.com>
2024-04-18 19:41:42 +0200

    Update bugfix

--- BugFixing/bugfix.py
+++ BugFixing/bugfix.py
@@ -6,39 +6,7 @@ import openai
 from utils.PreprocessUtils import addMixtralTokens
 
 
-repair_code_template = """You are a python expert in solving bugs:
-Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
-knowing that a fair judge decided that the python code under test is buggy.
-You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
-
-Method under test:
-{code}
-
-Description:
-{description}
-
-test case that produces an error:
-{test_case_error}
-
-the error message:
-{error_message}
-
-Explanation of a judge why the error is in the code under test:
-{explanation}
-
-You follow my rules and orders and if you do not know the answer, don't make things UP!
-You are going to follow the criteria that I give to you.
-Criteria:
-1. Understand the description and the python code under test.
-2. Interpret the test case that produces an error and the accompanying error message.
-3. Understand the explanation of a judge for why the error is in the code under test.
-3. Repair the code under test to be bug-free.
-4. make sure that the functionality of the repaired code conforms to the description.
-
-Your answer is a list of bugs found and the repaired code only.
-The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
-
-Judge_template = """You are python unit tester judge. Given the following python code, its description, a test case that produce an error and the error message.
+judge_template = """You are python unit tester judge. Given the following python code, its description, a test case that produce an error and the error message.
 You are going to follow the criteria that I give to you.
 You follow my rules and orders and if you do not know the answer, don't make things UP!
 Criteria:
@@ -71,4 +39,118 @@ Explanation: **EXPLANATION**
 Remember you are a judge and you should be fair and just. Do not make things up. Think before you end the response. I do not want any incomplete response.
 """
 
+repair_code_template = """You are a python expert in solving bugs:
+Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
+knowing that a fair judge decided that the python code under test is buggy.
+You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
 
+Method under test:
+{code}
+
+Description:
+{description}
+
+test case that produces an error:
+{test_case_error}
+
+the error message:
+{error_message}
+
+Explanation of a judge why the error is in the code under test:
+{explanation}
+
+You follow my rules and orders and if you do not know the answer, don't make things UP!
+You are going to follow the criteria that I give to you.
+Criteria:
+1. Understand the description and the python code under test.
+2. Interpret the test case that produces an error and the accompanying error message.
+3. Understand the explanation of a judge for why the error is in the code under test.
+3. Repair the code under test to be bug-free.
+4. make sure that the functionality of the repaired code conforms to the description.
+
+Your answer is a list of bugs found and the repaired code only.
+The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
+
+repair_code_with_fewShots_template = """You are a python expert in solving bugs:
+Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
+knowing that a fair judge decided that the python code under test is buggy.
+You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
+
+Method under test:
+{code}
+
+Description:
+{description}
+
+test case that produces an error:
+{test_case_error}
+
+the error message:
+{error_message}
+
+Explanation of a judge why the error is in the code under test:
+{explanation}
+
+I am going to provide you similar buggy functions and their corresponding bug fixes that you may need to use in your tests.
+{test_cases_of_few_shot}
+
+You follow my rules and orders and if you do not know the answer, don't make things UP!
+You are going to follow the criteria that I give to you.
+Criteria:
+1. Understand the description and the python code under test.
+2. Interpret the test case that produces an error and the accompanying error message.
+3. Understand the explanation of a judge for why the error is in the code under test.
+3. Repair the code under test to be bug-free.
+4. make sure that the functionality of the repaired code conforms to the description.
+
+Your answer is a list of bugs found and the repaired code only.
+The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
+
+
+def InitializeJudgeChain(llm):
+    global judge_template
+    
+    # adding [INST] to mixtral manually
+    if isinstance(llm, HuggingFaceHub) and "Mixtral" in llm.repo_id:
+        judge_template = addMixtralTokens(judge_template)
+        Take_decision_judge_template = addMixtralTokens(
+            judge_template
+        )
+    
+    Take_decision_judge_template = PromptTemplate(
+        template=judge_template,
+        input_variables=["description", "code", "test_case_error", "error_message"],
+        verbose=False,
+    )
+
+    JudgeCodeAndTestChain = LLMChain(
+        llm=llm, verbose=False, prompt=Take_decision_judge_template
+    )
+    return JudgeCodeAndTestChain
+
+def InitializeBugFixChain(llm, fewshots=False):
+    global repair_code_template
+    global repair_code_with_fewShots_template
+    # adding [INST] to mixtral manually
+    if isinstance(llm, HuggingFaceHub) and "Mixtral" in llm.repo_id:
+        repair_code_template = addMixtralTokens(repair_code_template)
+        repair_code_with_fewShots_template = addMixtralTokens(
+            repair_code_with_fewShots_template
+        )
+    if not fewshots:
+        Bug_Fix_Template = PromptTemplate(
+            template=repair_code_template,
+            input_variables=["description", "code", "test_case_error", "error_message", "explanation"],
+            verbose=False,
+        )
+    else:
+        Bug_Fix_Template = PromptTemplate(
+            template=repair_code_with_fewShots_template,
+            input_variables=["description", "code", "test_cases_of_few_shot"],
+            verbose=False,
+        )
+
+    BugFixChain = LLMChain(
+        llm=llm, verbose=False, prompt=Bug_Fix_Template
+    )
+    return BugFixChain
