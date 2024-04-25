00059a2cb1d9fba1281766e50d805d1a8edc668a
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2024-04-25 20:24:35 +0200

    fixer

--- PromptChains/FixBug.py
+++ PromptChains/FixBug.py
@@ -7,7 +7,7 @@ from utils.PreprocessUtils import addMixtralTokens
 
 
 repair_code_template = """You are a python expert in solving bugs:
-Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
+Given the following python code, its description, the error message and an explanation of a judge why the error is in the code under test.
 knowing that a fair judge decided that the python code under test is buggy.
 You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
 
@@ -16,9 +16,6 @@ Method under test:
 
 Description:
 {description}
-
-test case that produces an error:
-{test_case_error}
 
 the error message:
 {error_message}
@@ -30,10 +27,9 @@ You follow my rules and orders and if you do not know the answer, don't make thi
 You are going to follow the criteria that I give to you.
 Criteria:
 1. Understand the description and the python code under test.
-2. Interpret the test case that produces an error and the accompanying error message.
-3. Understand the explanation of a judge for why the error is in the code under test.
-4. Repair the code under test to be bug-free.
-5. Make sure that the functionality of the repaired code conforms to the description.
+2. Understand the explanation of a judge for why the error is in the code under test.
+3. Repair the code under test to be bug-free.
+4. Make sure that the functionality of the repaired code conforms to the description.
 
 Your answer is a list of bugs found and the repaired code only.
 The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
@@ -48,9 +44,6 @@ Method under test:
 
 Description:
 {description}
-
-test case that produces an error:
-{test_case_error}
 
 the error message:
 {error_message}
@@ -89,7 +82,6 @@ def InitializeBugFixChain(llm, fewshots=False):
             input_variables=[
                 "description",
                 "code",
-                "test_case_error",
                 "error_message",
                 "explanation",
             ],
