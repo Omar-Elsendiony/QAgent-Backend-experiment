65bc14749a956d71b1bdd45193001b71b5c987b6
karimyasser107 <karimyasser34@gmail.com>
2024-05-16 03:08:09 +0300

    created app.py

---
+++ Pipeline_Interface.py
@@ -0,0 +1,66 @@
+# def QAgent_setup():
+#     try:
+#         from .Configuration import *
+#         from .MainFunctions.TestGenerator import *
+#         from .MainFunctions.TestFix import *
+#         from .MainFunctions.DecisionMaker import *
+#         from .MainFunctions.BugFix import *
+
+#         print("All imports successful!")
+#         testGenerator = TestGenerator(GenUnitTestChain, db, globals())
+#         testRegenerator = TestFix(
+#             UnitTestFeedbackChain,
+#             globals(),
+#             True,
+#         )
+#         # judgeGenerator = DecisionMaker(judgeChain, globals())
+#         bugFixGenerator = BugFix(bugFixChain, globals(), True)
+#     except Exception as e:
+#         print(e)
+#         exit(-1)
+def QAgent_product(code, description):
+
+
+    # #TODO: IMPORTANT find a way to get the code and description from user later
+    # for now they are hardcoded
+
+
+    # code = """def add(a, b):\n    return a - b \n"""
+
+    # description = "This function adds two numbers"
+
+    isCodeBuggy = True
+
+    # print(isCodeBuggy)
+    # isCodeBuggy is accessible here
+
+    codeUnderTest, unitTestCode, feedbackParsed, testsToRepeat, isCodeBuggy = (
+        testGenerator.generate(code, description, isCodeBuggy)
+    )
+    print("Code Under Test: ", codeUnderTest)
+    print("Unit Test Code: ", unitTestCode)
+    print("Feedback Parsed: ", feedbackParsed)
+    print("Tests to Repeat: ", testsToRepeat)
+
+
+    # but isCodeBuggy is not accessible here
+
+    # if isCodeBuggy:
+    #     codeUnderTest, unitTestCode, feedbackParsed, testsToRepeat = (
+    #         bugFixGenerator.generate(
+    #             description, codeUnderTest, unitTestCode, testsToRepeat, feedbackParsed
+    #         )
+    #     )
+    # else:
+    #     codeUnderTest, unitTestCode, feedbackParsed, testsToRepeat = (
+    #         testRegenerator.generate(
+    #             description, codeUnderTest, unitTestCode, feedbackParsed
+    #         )
+    #     )
+
+    print("\n=============================================\nAfter Whichever: ")
+    print("Code Under Test: ", codeUnderTest)
+    print("Unit Test Code: ", unitTestCode)
+    print("Feedback Parsed: ", feedbackParsed)
+    print("Tests to Repeat: ", testsToRepeat)
+    return unitTestCode
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # import flask_cors
+from Pipeline_Interface import QAgent_product
+app = Flask(__name__)
+CORS(app)  # enable CORS
+try:
+    from .Configuration import *
+    from .MainFunctions.TestGenerator import *
+    from .MainFunctions.TestFix import *
+    from .MainFunctions.DecisionMaker import *
+    from .MainFunctions.BugFix import *
+
+    print("All imports successful!")
+    testGenerator = TestGenerator(GenUnitTestChain, db, globals())
+    testRegenerator = TestFix(
+        UnitTestFeedbackChain,
+        globals(),
+        True,
+    )
+    # judgeGenerator = DecisionMaker(judgeChain, globals())
+    bugFixGenerator = BugFix(bugFixChain, globals(), True)
+except Exception as e:
+    print(e)
+    exit(-1)
+@app.route('/run-qagentai', methods=['POST'])
+def run_python():
+    code = request.json.get('code')
+    description = request.json.get('description')
+    if code:
+        try:
+            # execute the main function
+            result = QAgent_product(code,description)
+            print(result)
+            return jsonify({'output': result})
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({'error': 'No code provided'}), 400
+
+if __name__ == '__main__':
+    app.run(port=5000)
