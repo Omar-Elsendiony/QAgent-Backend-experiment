dc53f8e9e8ce20d592f18e233213c58c50f80bef
Omar-Elsendiony <omar.sendiony@gmail.com>
2024-07-07 05:49:50 +0000

    bug fix reduce complications

--- PromptChains/FixBug.py
+++ PromptChains/FixBug.py
@@ -6,7 +6,7 @@ import openai
 from utils.PreprocessUtils import addMixtralTokens
 
 
-repair_code_template = """You are a python expert in solving bugs:
+repair_code_template2 = """You are a python expert in solving bugs:
 Given the following python code, its description, a test case that produce an error, the error message.
 Knowing that the python code under test is buggy.
 You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
@@ -34,7 +34,7 @@ Criteria:
 Your answer is a list of bugs found and the repaired code only.
 The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
 
-repair_code_with_fewShots_template = """You are a python expert in solving bugs:
+repair_code_with_fewShots_template2 = """You are a python expert in solving bugs:
 Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
 knowing that a fair judge decided that the python code under test is buggy.
 You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
@@ -65,6 +65,61 @@ Criteria:
 
 Your answer is a list of bugs found and the repaired code only.
 The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
+
+###################### SECOND CASES ##########################
+repair_code_template = """You are a python expert in solving bugs:
+Given the following python code, its description, a test case that produce an error, the error message.
+Knowing that the python code under test is buggy.
+You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
+Method under test:
+{code}
+
+Description:
+{description}
+
+test case that produces an error:
+{test_case_error}
+
+the error message:
+{error_message}
+
+
+Understand the description and the python code under test.
+Interpret the test case that produces an error and the accompanying error message.
+Understand the explanation of a judge for why the error is in the code under test.
+Repair the code under test to be bug-free.
+
+Your answer is a list of bugs found and the repaired code only.
+The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
+
+repair_code_with_fewShots_template = """You are a python expert in solving bugs:
+Given the following python code, its description, a test case that produce an error, the error message and an explanation of a judge why the error is in the code under test.
+knowing that a fair judge decided that the python code under test is buggy.
+You are required to repair the code under test to be bugless and make sure that the functionality conform with the description provided.
+Method under test:
+{code}
+
+Description:
+{description}
+
+the error message:
+{error_message}
+
+Explanation of a judge why the error is in the code under test:
+{explanation}
+
+I am going to provide you similar buggy functions and their corresponding bug fixes that you may need to use in your tests.
+{test_cases_of_few_shot}
+
+Understand the description and the python code under test.
+Interpret the test case that produces an error and the accompanying error message.
+Understand the explanation of a judge for why the error is in the code under test.
+Repair the code under test to be bug-free.
+
+Your answer is a list of bugs found and the repaired code only.
+The repaired code should be a markdown code snippet formatted in the following schema, including the leading and trailing "```python" and "```" respectively:"""
+
+
 
 
 def InitializeBugFixChain(llm, fewshots=False):
